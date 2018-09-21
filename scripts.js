6ab97a8abcbe58ef1172281ba1944ee88133129a
Manny Navarro <manny.navarro@gmail.com>
2018-09-21 16:21:03 -0700

    added jQuery scripts to access images, working, but need to add some sort of button to scroll through images

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@
 $(function(){
     $.getJSON('cars.json', function(cars){
 
-        var template=$()
+        var template = $('#carimagestemplate').html();
+        var html=Mustache.to_html(template, cars);
+        $('.carimages').html(html);
 
 
     });
