427116f20a35603db8ef9c6a8e483023b6572392
Manny Navarro <manny.navarro@gmail.com>
2018-09-21 16:30:17 -0700

    added jQuery cycle plugin and effects in order to cycle through images

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 'use strict';
 $(function(){
+    //using jQuery to access the cars JSON
     $.getJSON('cars.json', function(cars){
-
+        //grabbing images and sending to hmtl div for display
         var template = $('#carimagestemplate').html();
-        var html=Mustache.to_html(template, cars);
+        var html = Mustache.to_html(template, cars);
         $('.carimages').html(html);
+        //targeting carimages again using cycle plugin (jQuery for slider)
+        $('.carimages').cycle({
+            //desired effects for using the jQuery cycle plugin
+            fx : 'fade',
+            pause: 1,
+            next: '#next',
+            prev: '#prev',
+            speed: 1000,
+            timeout: 1000
 
-
+        });
     });
-
 });
